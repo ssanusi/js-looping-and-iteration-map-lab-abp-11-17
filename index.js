9dac7339619e7b0581f9ea296f5efd6d94b9b432
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-11 11:50:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function nameToAttributes(drivers) {
   return drivers.map(e => ({firstName:e.split(' ')[0],lastName:e.split(' ')[1]}))
 
 }
+function attributesToPhrase(drivers) {
+  return drivers.map(e => `${e[name]} is from ${e[hometown]}`)
+  
+}
