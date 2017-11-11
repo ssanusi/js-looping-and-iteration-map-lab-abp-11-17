4d0c10fb2d1b01fc5f3d0069a96e47ae01e5541d
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-11 11:42:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function lowerCaseDrivers(drivers) {
   return drivers.map(e => e.toLowerCase());
 }
 function nameToAttributes(drivers) {
-  return drivers.map(e => {firstName:e.split(' ')[0],lastName:e.split(' ')[1]})
-  
+  return drivers.map(e => ({firstName:e.split(' ')[0],lastName:e.split(' ')[1]}))
+
 }
